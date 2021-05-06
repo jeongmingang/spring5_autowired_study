34290e73950deb749cf651f2a97b6cf9955e9451
mingang <j01036733521@gmail.com>
2021-05-06 15:04:52 +0900

    초기화

---
+++ src/main/java/spring5_autowired_study/spring/ChangePasswordService.java
@@ -0,0 +1,30 @@
+package spring5_autowired_study.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ChangePasswordService {
+	@Autowired
+	private MemberDao memberDao;
+	
+	public ChangePasswordService() {
+		
+	}
+	
+//	public ChangePasswordService(MemberDao memberDao) {
+//		this.memberDao = memberDao;
+//	}
+
+	public void changePassword(String email, String oldPwd, String newPwd) {
+		Member member = memberDao.selectByEmail(email);
+		if(member == null) {
+			throw new MemberNotFoundException();
+		}	
+		member.changePassword(oldPwd, newPwd);
+		
+		memberDao.update(member);
+	}
+
+//	public void setMemberDao(MemberDao memberDao) {
+//		this.memberDao = memberDao;
+//	}
+}
